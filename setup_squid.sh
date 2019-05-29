3f2b8b5698de4d7525538be8395daeb2c0e6e1e5
mute1997 <mute1008@gmail.com>
2019-05-29 17:59:26 +0900

    fix

--- setup_squid.sh
+++ setup_squid.sh
@@ -8,17 +8,17 @@ eth1_base=10.1.0.1
 wget https://raw.githubusercontent.com/tests-always-included/mo/master/mo
 chmod 744 mo
 
-sudo apt update
-sudo apt install squid
+sudo apt -y update
+sudo apt -y install squid
 sudo systemctl enable squid
 
 sudo echo "1 rt1" | sudo tee -a /etc/iproute2/rt_tables
 
 ETH1_BASE=${eth1_base} ETH1_IP=${eth1} ./mo interfaces > i
-HOME=${home} ETH0=${eth0} ETH1=${eht1} ./mo squid.conf > s
+HOME=${home} ETH0=${eth0} ETH1=${eth1} ./mo squid.conf > s
 sudo cp i /etc/network/interfaces
 sudo cp s /etc/squid/squid.conf
 
 sudo sed -ie 's/setup = .*/setup = false/g' /etc/default/instance_configs.cfg
 
-sudo reboot
+#sudo reboot
